93c7a8ab461aa5b211570e6777eaa1b7f18c2085
Kevin Irias <kiriasru@gmail.com>
2023-10-01 18:18:57 -0600

    Se ha agregado la ruta con tareas incompletas

--- list-view-router.js
+++ list-view-router.js
@@ -5,22 +5,46 @@ router.get('/completed-tasks', (req, res) => {
     res.json([{
         id: 1,
         Tarea: "Estudiar HTML",
-        Estado: "Completada"
+        Estado: "Completa"
     },
     {
         id: 2,
         Tarea: "Estudiar HTML",
-        Estado: "Completada"
+        Estado: "Completa"
     },
     {
         id: 3,
         Tarea: "Avanzar en la plataforma",
-        Estado: "Completada"
+        Estado: "Completa"
     },
     {
         id: 4,
         Tarea: "Estudiar Express",
-        Estado: "Completada"
+        Estado: "Completa"
+    },
+    ]);
+});
+
+router.get('/incomplete-tasks', (req, res) => {
+    res.json([{
+        id: 1,
+        Tarea: "Estudiar Node.js",
+        Estado: "Incompleta"
+    },
+    {
+        id: 2,
+        Tarea: "Estudiar JavaScript",
+        Estado: "Incompleta"
+    },
+    {
+        id: 3,
+        Tarea: "Terminar laboratorio de Express",
+        Estado: "Incompleta"
+    },
+    {
+        id: 4,
+        Tarea: "Repasar enrutamiento",
+        Estado: "Incompleta"
     },
     ]);
 });
